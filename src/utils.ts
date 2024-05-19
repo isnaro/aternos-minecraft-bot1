4650212591ce546b0f3dcebcf990492e16a572e4
NB <nasrobegg@gmail.com>
2024-05-19 22:04:15 +0100

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,8 @@
-export const sleep = (ms: number): Promise<NodeJS.Timeout> => new Promise(resovle => setTimeout(resovle, ms));
+export const sleep = (ms: number): Promise<void> => new Promise(resolve => setTimeout(resolve, ms));
 
-export const getRandom = <T>(arr: T[]): T => arr[Math.floor(Math.random() * arr.length)];
+export const getRandom = <T>(arr: T[]): T => {
+    if (arr.length === 0) {
+        throw new Error("Cannot get a random element from an empty array");
+    }
+    return arr[Math.floor(Math.random() * arr.length)];
+};
